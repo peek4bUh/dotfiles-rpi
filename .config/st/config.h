7395a7a34c2aa3ab6bbbd22820a405e2d555f7b3
Angel Andrade <aandradeb99@gmail.com>
2022-08-03 15:54:34 +0200

    [dwm / st] Change to Roboto Mono Nerd Font

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=28:antialias=true:autohint=true";
+static char *font = "RobotoMono Nerd Font:style=Regular:size=18";
 static int borderpx = 2;
 
 /*
