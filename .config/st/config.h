024b9fd0afd5c3150bb6575fde2d142ed7a135f1
Angel Andrade <aandradeb99@gmail.com>
2022-08-17 16:09:45 +0200

    [st] Add 10 pixels of padding

--- .config/st/config.h
+++ .config/st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "RobotoMono Nerd Font:style=Regular:size=18";
-static int borderpx = 2;
+static int borderpx = 10;
 
 /*
  * What program is execed by st depends of these precedence rules:
