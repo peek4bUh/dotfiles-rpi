3678f553912d06593170a852bb57be05588a03ad
peek4bUh <aandradeb99@gmail.com>
2025-05-04 15:12:41 +0200

    Move bar to bottom

--- pi/.config/dwm/config.h
+++ pi/.config/dwm/config.h
@@ -5,9 +5,9 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 10;        /* 2 is the default spacing around the bar's font */
-static const char *fonts[]          = { "RobotoMono Nerd Font:style=Regular:size=20" };
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const int user_bh            = 24;       /* 2 is the default spacing around the bar's font */
+static const char *fonts[]          = { "RobotoMono Nerd Font:style=Regular:size=18" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
